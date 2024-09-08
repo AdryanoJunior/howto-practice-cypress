33dcff8ea58baa41aec9c1023b21659cf6594af4
Adryano Junior <jusamsung10@gmail.com>
2024-09-08 13:43:23 -0300

    Added selectors list for login spec

--- cypress/e2e/loginguide.spec.cy.js
+++ cypress/e2e/loginguide.spec.cy.js
@@ -2,19 +2,27 @@ describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/my-account/')  
   });
+
+  const selectorsList = {
+    usernameField : '#username',
+    passwordField : '#password',
+    loginButton : '.woocommerce-form > .button',
+    selectionTitleMyAccount: '.woocommerce-MyAccount-content > :nth-child(2)', 
+    wrongCredentialAlert:'.woocommerce-error'
+  }
   
     it('Login - Sucess', () => {
-      cy.get('#username').type('testeqasenior@gmail.com') 
-      cy.get('#password').type('qasenior78')
-      cy.get('.woocommerce-form > .button').click()
+      cy.get(selectorsList.usernameField).type('testeqasenior@gmail.com') 
+      cy.get(selectorsList.passwordField).type('qasenior78')
+      cy.get(selectorsList.loginButton).click()
       cy.location('pathname').should('equal', '/my-account/')
-      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').contains('Olá, testeqasenior (não é testeqasenior? Sair)')
+      cy.get(selectorsList.selectionTitleMyAccount).contains('Olá, testeqasenior (não é testeqasenior? Sair)')
     })
   
     it('Login - Fail', () => {
-      cy.get('#username').type('teste2qasenior@gmail.com') 
-      cy.get('#password').type('qasenior789')
-      cy.get('.woocommerce-form > .button').click()
-      cy.get('.woocommerce-error').contains('Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+      cy.get(selectorsList.usernameField).type('teste2qasenior@gmail.com') 
+      cy.get(selectorsList.passwordField).type('qasenior789')
+      cy.get(selectorsList.loginButton).click()
+      cy.get(selectorsList.wrongCredentialAlert).contains('Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     });
   })
